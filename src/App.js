1f05ca9eaa0d370a4307335b1e854ac9f6a4a6ba
Joe <joe@joeebner.com>
2026-02-19 14:06:04 +0700

    fixed import path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@
 
 import React, { useState, useEffect } from 'react';
 import './components/NWRSWebsite.css';
-import AvailabilityCalendar from './AvailabilityCalendar';
+import AvailabilityCalendar from './components/AvailabilityCalendar';
 
 function NWRSWebsite() {
 	useEffect(() => {
